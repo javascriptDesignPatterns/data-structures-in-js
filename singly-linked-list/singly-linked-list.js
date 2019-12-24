9909e720080f2b269d26475fd488497b7aa08a16
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2019-12-24 16:49:20 +0530

    [SLL] Add unshift()

--- singly-linked-list/singly-linked-list.js
+++ singly-linked-list/singly-linked-list.js
@@ -54,6 +54,17 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head.next;
+      this.head = newNode;
+    }
+  }
 }
 
 let sll = new SinglyLinkedList();
